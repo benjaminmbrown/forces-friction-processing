97c73d8b5682192b8e5e3f6ff2f4ccd4fe4c45f2
Benjamin M. Brown <benjaminbb@gmail.com>
2016-04-09 15:14:06 -0500

    adding friction

---
+++ README.md
@@ -0,0 +1,3 @@
+# forces-many-objects
+Example of applying additive friction forces to many objects of varied mass in Processing
+<img src= "https://media.giphy.com/media/3o85gdL1W6sPJLqRAQ/giphy.gif"/>
---
+++ sketch.js
@@ -0,0 +1,46 @@
+var movers=[];
+var noiseScale = 0.001;
+
+function setup() {
+  createCanvas(440, 260);
+  //m = new Mover(random(0.1, 5), 0, 0)
+
+    for (var i = 0; i < 30; i++) {
+      movers[i] = new Mover(random(0.1,5),0,0);
+  }
+}
+
+function draw() {
+  background(51);
+  //noise() takes  params value, valRangeMin, valRangeMax, desiredRangeMin,desiredRangeMax
+  var wind = createVector(0.01, 0);
+
+
+  for (var i = 0; i < movers.length; i++) {
+    //move gravity into here so we can apply mass of each object to it
+    var gravity = createVector(0, 0.1*movers[i].mass);
+
+    var frictionCoefficient = 0.01; //higher coefficient = more friction / slower surface
+    var normal = 1;//normal force 
+    var frictionMag = frictionCoefficient * normal; // magnitude of friction
+    var friction = movers[i].velocity.copy();//make a copy b/c we don't want to change the directions
+    friction.mult(-1);// friction force acts opposite vector of it's velocity
+    friction.normalize();
+    friction.mult(frictionMag);
+
+     movers[i].applyForce(wind);
+     movers[i].applyForce(gravity);
+     movers[i].applyForce(friction);
+     movers[i].update();
+     movers[i].display();
+     movers[i].checkEdges();
+
+  }
+  
+
+noiseScale+=0.001;
+
+
+}
+
+
